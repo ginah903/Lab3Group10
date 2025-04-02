ea54405b3070058fe64e884574b3adfab361c502
jackeveker <jackeveker@jacks-air.localdomain>
2025-04-02 16:26:08 -0500

    Added ElectricBassGuitar class in abstractfactory.instrument

---
+++ src/abstractfactory/instrument/ElectricBassGuitar.java
@@ -0,0 +1,22 @@
+package abstractfactory.instrument;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+public class ElectricBassGuitar implements InstrumentStrategy{
+
+	@Override
+	public void applyInstrument(Track track, int channel) {
+		ShortMessage programChange = new ShortMessage();
+		try {
+			programChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, 33, 0);
+		} catch (InvalidMidiDataException e) {
+			e.printStackTrace();
+		}
+		MidiEvent event = new MidiEvent(programChange, 0);
+		track.add(event);
+	}
+
+}
