c1f00b804ababc359132b15a5764089564968ae2
ginah903 <ginah903@gmail.com>
2025-04-02 12:25:49 -0500

    Pushing LegatoMidiEventFactory without javadoc

---
+++ src/abstractfactory/behavior/LegatoMidiEventFactory.java
@@ -0,0 +1,25 @@
+package abstractfactory.behavior;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+import abstractfactory.MidiEventFactory;
+
+
+public class LegatoMidiEventFactory implements MidiEventFactory {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		return new MidiEvent(message, tick);
+		
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_OFF, channel, note);
+		return new MidiEvent(message, tick+80);
+	}
+	
+}
