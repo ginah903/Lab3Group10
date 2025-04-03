37ebc755f65d3ff80e37b60ae1fb39a70ccf2a63
jackeveker <jackeveker@jacks-air.localdomain>
2025-04-02 19:23:56 -0500

    added MidiCsvParser (work in progress)

--- src/parser/MidiCsvParser.java
+++ src/parser/MidiCsvParser.java
@@ -1,5 +1,38 @@
 package parser;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MidiCsvParser {
 
+	public List<MidiEventData> parsedMidiEvent(String filename) {
+		
+		List<MidiEventData> events = new ArrayList<>();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+			String line;
+			
+			while((line = br.readLine())!= null) {
+				String[] values = line.split(",");
+				
+				if(values.length == 6) {
+					//parse the values as integers
+					//convert Note on/off to integers 1 or 0
+					//create MidiEventData object
+					//not done yet!!!!!
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return events;
+	}
 }
