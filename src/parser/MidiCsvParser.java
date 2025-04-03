1658d5a22152a33185af4187dbc1791d15971d11
rai8d4 <122903297+rai8d4@users.noreply.github.com>
2025-04-03 12:57:18 -0500

    Completed CSV parser and created unique MidiEventData event per CSV line

--- src/parser/MidiCsvParser.java
+++ src/parser/MidiCsvParser.java
@@ -1,6 +1,7 @@
 package parser;
 
 import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -20,12 +21,31 @@ public class MidiCsvParser {
 				String[] values = line.split(",");
 				
 				if(values.length == 6) {
-					//parse the values as integers
-					//convert Note on/off to integers 1 or 0
-					//create MidiEventData object
-					//not done yet!!!!!
+					
+					//pulling values separated by commas here
+					
+					int startEndTick = Integer.parseInt(values[0].trim());
+					
+                    String noteType = values[1].trim();
+                    
+                    int noteOnOff = noteType.equalsIgnoreCase("Note_on_c") ? 1 : 0;
+                    
+                    int channel = Integer.parseInt(values[2].trim());
+                    
+                    int note = Integer.parseInt(values[3].trim());
+                    
+                    int velocity = Integer.parseInt(values[4].trim());
+                    
+                    int instrument = Integer.parseInt(values[5].trim());
+                    
+                    MidiEventData event = new MidiEventData(
+                    	startEndTick, velocity, note, channel, instrument, noteOnOff
+                    );
+                    
+                    events.add(event);
 				}
 			}
+			
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
